ad984e14dac3cbf02eee6896d3714587f16638b1
Yasin <guneyyasin.6797@outlook.com>
2024-02-13 17:47:19 +0300

    React öğreniyorum

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo}></img>
+    </div>
+  );
+}
+
+export default App;
